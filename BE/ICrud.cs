e31f9aa22f839a3f52bc3b054be6fb2c9a36eebd
Facundo Planella <50037315+FacundoPlanella@users.noreply.github.com>
2022-12-02 17:32:44 -0300

    Add files via upload

---
+++ BE/ICrud.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public interface ICrud <T>
+    {
+        bool Create(T objAdd);
+        List<T> GetAll();
+        T GetById(T objId);
+        bool Update(T objUpd);
+        bool Delete(T objUdp);
+
+
+    
+    }
+}
